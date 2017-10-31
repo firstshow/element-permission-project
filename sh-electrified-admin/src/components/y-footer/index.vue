12265960d78be2ca9fb7fbfc2169ab97a9cbeef6
zhangcheng <776331229@qq.com>
2017-10-31 14:12:08 +0800

    重新搭间

--- sh-electrified-admin/src/components/y-footer/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <footer class="footer-box text-center padding-top-50">
-        <p>Copyright © 2014 付呗 - 浙ICP备13030782号-4 地址：杭州市西湖区池华街与紫萱路交叉口丰盛九玺东区12幢18-22层</p>
-    </footer>
-</template>
-
-<style lang="less" scoped>
-    @import "index.less";
-</style>
--- sh-electrified-admin/src/vuex/loginInfo/getters.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by guoxiaolin on 2017/6/22.
- */
-
-/**
- * 获取注册信息
- */
-
-export const registerInfo = state => state.registerInfo;
