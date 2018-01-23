f0ed69445c0b8f943e5e9018cfc7dd4d7a554049
zhangcheng <776331229@qq.com>
2018-01-23 16:06:51 +0800

    封装 axios55

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -6,7 +6,7 @@
       <form action="" class="login-form">
         <div class="login-form-title">用户登录1111111</div>
         <div class="login-form-title">用户登录1111111</div>
-        <div class="login-form-title">用户登录11111133</div>
+        <div class="login-form-title">用户登录1111113333333333</div>
         <div class="login-input-item ">
           <label for="username" class="people-icon " style="margin-right: 9px;"></label>
           <input type="text" id="username" v-model="username" class="login-input" placeholder="请输入用户名">
