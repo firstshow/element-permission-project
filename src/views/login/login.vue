545d23231899be1da403d162a172c846a58bf37d
zhangcheng <776331229@qq.com>
2018-01-23 16:01:19 +0800

    封装 axios11

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -4,7 +4,7 @@
     <div class="login-form-box ">
       <div class="login-title">生活缴费总后台</div>
       <form action="" class="login-form">
-        <div class="login-form-title">用户登录</div>
+        <div class="login-form-title">用户登录1111111</div>
         <div class="login-input-item ">
           <label for="username" class="people-icon " style="margin-right: 9px;"></label>
           <input type="text" id="username" v-model="username" class="login-input" placeholder="请输入用户名">
